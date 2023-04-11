7efa25c4d78b70bf71ca5bb6b9d3ec02ae7f8207
ovommmHJ <killos033@gmail.com>
2023-04-11 20:49:33 +0900

    feat : setting

---
+++ src/main/java/com/moa/moa/ConcertMoaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moa.moa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertMoaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertMoaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moa/moa/ConcertMoaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moa.moa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertMoaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
